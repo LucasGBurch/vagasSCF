10f1a25ae064c3a89a2e4e3cc7cdfa04e6736219
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-06-11 10:32:36 -0300

    Solucao teste1

--- teste1.js
+++ teste1.js
@@ -1,24 +1,26 @@
-var data =  require("./fakeData");
+var data = require('./fakeData');
 
-const getUser = ( req, res, next ) => {
-    
-    var name =  req.query.name;
+const getUser = (req, res, next) => {
+  // SOLUÇÃO ANTERIOR
+  // var name = req.query.name;
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            res.send(data[i]);
-        }
-    }
+  // for(let i = 0; i < data.length;  i++) {
+  //     if(i.name == name) {
+  //         res.send(data[i]);
+  //     }
+  // }
 
+  // SOLUÇÃO NOVA COM FIND:
+  let id = req.query.id;
+  const dados = data.find((data) => data.id === id);
+  res.send(dados);
 };
 
-const getUsers = ( req, res, next ) => {
-    
-    res.send(data);
-    
+const getUsers = (req, res, next) => {
+  res.send(data);
 };
 
 module.exports = {
-    getUser,
-    getUsers
+  getUser,
+  getUsers,
 };
