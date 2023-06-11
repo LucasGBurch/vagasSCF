b6745f346b352a0ff63d86c7b1f2e324097dbbd7
Lucas Burch <88804181+LucasGBurch@users.noreply.github.com>
2023-06-11 15:43:41 -0300

    Solucao delete do teste3

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.get('/', function (req, res) {
 app.get('/user/:id', teste1.getUser);
 app.get('/users', teste1.getUsers);
 app.post('/users', teste2.postUser);
-app.delete('/users', teste3);
+app.delete('/users/:id', teste3.deleteUser);
 app.put('/users', teste4);
 app.get('/users/access', teste5);
 
--- teste3.js
+++ teste3.js
@@ -1,15 +1,22 @@
-var data =  require("./fakeData");
+var data = require('./fakeData');
 
-module.exports = function(req, res) {
-  
-    var name =  req.query.name;
+const deleteUser = (req, res) => {
+  // SOLUÇÃO ANTERIOR (aqui estava deletando todo o array):
+  //   var name = req.query.name;
+  //   for (let i = 0; i < data.length; i++) {
+  //     if (i.name == name) {
+  //       data[i] = null;
+  //     }
+  //   }
 
-    for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            data[i] = null;
-        }
-    }
+  // Solução nova: buscando pela posição e deletando do array
+  let id = +req.params.id;
+  const position = data.findIndex((usuario) => usuario.id === id);
+  delete data[position];
 
-    res.send("success");
+  res.send(`Usuário de id ${id} deletado da base.`);
+};
 
+module.exports = {
+  deleteUser,
 };
